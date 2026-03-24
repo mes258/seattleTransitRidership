73e8c7776d9593909eba8271b090b092fb5c3ffc
mes258 <michael.smith.singapore@gmail.com>
2026-03-24 12:51:43 -0700

    agency ridership data function

--- backend/app/accessModule.py
+++ backend/app/accessModule.py
@@ -8,3 +8,16 @@ def getRidershipData(agency, routeNum, servicePeriod):
 
 # For testing
 # print(getRidershipData("kcm", "7", "243"))
+
+# Uncomment to print out the results on startup.
+# def getAgencyRidership(agency, servicePeriod):
+#     for i in range(0, 1000):
+#         routeId = str(i)
+#         try:
+#           df = getRidershipData(agency, routeId, servicePeriod)
+#           avgDailyRidershipPerRoute = int(df["dailyBoardings"].sum())
+#           print("{0}, {1}".format(routeId, avgDailyRidershipPerRoute))
+#         except:
+#           continue
+
+# getAgencyRidership("ct", "2510")
